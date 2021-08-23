4bd298f11e5f536ae3356e5e5096d56afcecee8a
Sstandby <standbyy9@gmail.com>
2021-08-23 01:05:45 -0500

    Cambiando tipo de dato

--- models/champions-model.js
+++ models/champions-model.js
@@ -5,7 +5,7 @@ var championSchema = new Schema({
     Nombre: { type: String, required: false},
     Descripción: { type: String, required: false },
     Tipo: { type: String, required: false },
-    Vida_inicial: { type: String, required: false }
+    Vida_inicial: { type: Number, required: false }
 })
 
 var Champion = mongoose.model('Champion', championSchema)
